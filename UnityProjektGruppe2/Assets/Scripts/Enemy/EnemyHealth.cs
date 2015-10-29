afb378870fd697b3c07c1a10b54a3f7804fd495b
Nikolaj Sloth <keysar50@hotmail.com>
2015-10-29 13:21:00 +0100

    Enemy kan "synke" efter death, basis for Enemy hitparticles on side hit

--- UnityProjektGruppe2/Assets/Scripts/Enemy/EnemyHealth.cs
+++ UnityProjektGruppe2/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -4,17 +4,17 @@ public class EnemyHealth : MonoBehaviour
 {
 	public int startingHealth = 100;            // The amount of health the enemy starts the game with.
 	public int currentHealth;                   // The current health the enemy has.
-	//public float sinkSpeed = 2.5f;              // The speed at which the enemy sinks through the floor when dead.
+	public float sinkSpeed = 2.5f;              // The speed at which the enemy sinks through the floor when dead.
 	//public int scoreValue = 10;                 // The amount added to the player's score when the enemy dies.
 	//public AudioClip deathClip;                 // The sound to play when the enemy dies.
 	
 	
 	Animator anim;                              // Reference to the animator.
 	//AudioSource enemyAudio;                     // Reference to the audio source.
-	//ParticleSystem hitParticles;                // Reference to the particle system that plays when the enemy is damaged.
+	ParticleSystem hitParticles;                // Reference to the particle system that plays when the enemy is damaged.
 	CapsuleCollider capsuleCollider;            // Reference to the capsule collider.
 	bool isDead;                                // Whether the enemy is dead.
-	//bool isSinking;                             // Whether the enemy has started sinking through the floor.
+	bool isSinking;                             // Whether the enemy has started sinking through the floor.
 	
 	
 	void Awake ()
@@ -22,7 +22,7 @@ public class EnemyHealth : MonoBehaviour
 		// Setting up the references.
 		anim = GetComponent <Animator> ();
 		//enemyAudio = GetComponent <AudioSource> ();
-		//hitParticles = GetComponentInChildren <ParticleSystem> ();
+		hitParticles = GetComponentInChildren <ParticleSystem> ();
 		capsuleCollider = GetComponent <CapsuleCollider> ();
 		
 		// Setting the current health when the enemy first spawns.
@@ -31,12 +31,12 @@ public class EnemyHealth : MonoBehaviour
 	
 	void Update ()
 	{
-		// If the enemy should be sinking...
-        //if(isSinking)
-        //{
-        //    // ... move the enemy down by the sinkSpeed per second.
-        //    transform.Translate (-Vector3.up * sinkSpeed * Time.deltaTime);
-        //}
+         //If the enemy should be sinking...
+        if(isSinking)
+        {
+            // ... move the enemy down by the sinkSpeed per second.
+            transform.Translate (-Vector3.up * sinkSpeed * Time.deltaTime);
+        }
 	}
 	
 	
@@ -55,10 +55,10 @@ public class EnemyHealth : MonoBehaviour
 		currentHealth -= amount;
 		
 		// Set the position of the particle system to where the hit was sustained.
-		//hitParticles.transform.position = hitPoint;
+		hitParticles.transform.position = hitPoint;
 		
 		// And play the particles.
-		//hitParticles.Play();
+		hitParticles.Play();
 		
 		// If the current health is less than or equal to zero...
 		if(currentHealth <= 0)
@@ -95,7 +95,7 @@ public class EnemyHealth : MonoBehaviour
 		GetComponent <Rigidbody> ().isKinematic = true;
 		
 		// The enemy should no sink.
-		//isSinking = true;
+		isSinking = true;
 		
 		// Increase the score by the enemy's score value.
 		//ScoreManager.score += scoreValue;
